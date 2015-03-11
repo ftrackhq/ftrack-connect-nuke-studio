b3d5ef6bb58005a3690d4b50b0c1797001b37bca
langeli <lorenzo.angeli@efestolab.uk>
2015-03-11 11:44:51 +0100

    inherit manager from ftrack_connect_nuke
    move imports after host type check

--- source/ftrack_connect_nuke_studio/manager.py
+++ source/ftrack_connect_nuke_studio/manager.py
@@ -1,24 +1,26 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2014 ftrack
-
-from hiero.core import events
-
-
-from ftrack_connect_foundry import manager
-from ftrack_connect_nuke_studio.ui.tag_manager import TagManager
-from ftrack_connect_nuke_studio.ui.tag_drop_handler import TagDropHandler
-
+import FnAssetAPI
+from ftrack_connect_nuke import manager
 
 class ManagerInterface(manager.ManagerInterface):
     def initialize(self):
         '''Prepare for interaction with the current host.'''
         super(ManagerInterface, self).initialize()
-        self.tag_handler = TagDropHandler()
 
-        # after the project load , create the ftrack tags.
-        events.registerInterest(
-            'kStartup',
-            TagManager
-        )
+        host = FnAssetAPI.SessionManager.currentSession().getHost()
+        if host and host.getIdentifier() == 'uk.co.foundry.nukestudio':
+
+            from hiero.core import events
+            from ftrack_connect_nuke_studio.ui.tag_manager import TagManager
+            from ftrack_connect_nuke_studio.ui.tag_drop_handler import TagDropHandler
+
+            self.tag_handler = TagDropHandler()
+
+            # after the project load , create the ftrack tags.
+            events.registerInterest(
+                'kStartup',
+                TagManager
+            )
 
 
