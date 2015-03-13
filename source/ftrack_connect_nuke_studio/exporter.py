33c48b94db403b1f00694318e3e12a07eb719064
langeli <lorenzo.angeli@efestolab.uk>
2015-03-13 19:49:21 +0100

    reformat code to better pep8

--- source/ftrack_connect_nuke_studio/exporter.py
+++ source/ftrack_connect_nuke_studio/exporter.py
@@ -5,93 +5,91 @@ import FnAssetAPI
 
 OriginalScriptWriter = hiero.core.nuke.ScriptWriter
 
+
 class CustomScriptWriter(OriginalScriptWriter):
-  result_outputs = {}
+    result_outputs = {}
 
-  def __init__(self, entity_reference):
-    OriginalScriptWriter.__init__(self)
-    self.entity_reference = entity_reference
+    def __init__(self, entity_reference):
+        OriginalScriptWriter.__init__(self)
+        self.entity_reference = entity_reference
 
+    def addNode(self, node):
+        # List of nodes that should actually be added to the script
+        nodesToAdd = []
 
-  def addNode(self, node):
-    # List of nodes that should actually be added to the script
-    nodesToAdd = []
+        # node might actually be a list of nodes.  If it is, call onNodeAdded
+        # for each one
+        if isinstance(node, hiero.core.nuke.Node):
+            nodesToAdd.append(self.onNodeAdded(node))
+        else:
+            try:
+                for n in node:
+                    nodesToAdd.append(self.onNodeAdded(n))
+            except:
+                pass
 
-    # node might actually be a list of nodes.  If it is, call onNodeAdded for each one
-    if isinstance(node, hiero.core.nuke.Node):
-      nodesToAdd.append( self.onNodeAdded(node) )
-    else:
-      try:
-        for n in node:
-          nodesToAdd.append( self.onNodeAdded(n) )
-      except:
-        pass
+        # Call base class to add the node(s)
+        OriginalScriptWriter.addNode(self, nodesToAdd)
 
-    # Call base class to add the node(s)
-    OriginalScriptWriter.addNode(self, nodesToAdd)
+    def onNodeAdded(self, node):
+        """ Callback when a node is added. Return the node that should actually be added. """
+        if node.type() == "Read":
+            # print "CustomScriptWriter Read node added:", node.knob("file")
+            # Replace the path with a Python expression
+            # newFileKnobValue = "\\[python str('%s')\\]" %  node.knob("file")
+            node.setKnob("file", self.entity_reference)
 
+        elif node.type() == "Write":
+            # print "CustomScriptWriter Write node added:", node.knob("file")
+            # Replace the Write node with MyWrite, copying all the knob values
+            newNode = hiero.core.nuke.Node("MyWrite", **node.knobs())
+            node = newNode
 
-  def onNodeAdded(self, node):
-    """ Callback when a node is added. Return the node that should actually be added. """
-    if node.type() == "Read":
-      # print "CustomScriptWriter Read node added:", node.knob("file")
-      # Replace the path with a Python expression
-      # newFileKnobValue = "\\[python str('%s')\\]" %  node.knob("file")
-      node.setKnob("file", self.entity_reference)
+        return node
 
-    elif node.type() == "Write":
-      # print "CustomScriptWriter Write node added:", node.knob("file")
-      # Replace the Write node with MyWrite, copying all the knob values
-      newNode = hiero.core.nuke.Node("MyWrite", **node.knobs())
-      node = newNode
-
-    return node
-
-  def writeToDisk(self, scriptFilename):
-    """ Write the script. """
-    key = os.path.splitext(os.path.basename(scriptFilename))[0].split('_')[-2]
-    self.result_outputs[key] = scriptFilename
-    OriginalScriptWriter.writeToDisk(self, scriptFilename)
+    def writeToDisk(self, scriptFilename):
+        """ Write the script. """
+        key = os.path.splitext(
+            os.path.basename(scriptFilename))[0].split('_')[-2]
+        self.result_outputs[key] = scriptFilename
+        OriginalScriptWriter.writeToDisk(self, scriptFilename)
 
 
 def export(trackItem, presetName="Basic Nuke Shot With Annotations"):
-  current_preset = None
-  exportPresetName = presetName
-  for preset in hiero.core.taskRegistry.localPresets():
-    if preset.name() == exportPresetName:
-      current_preset = preset
-      break
+    current_preset = None
+    exportPresetName = presetName
+    for preset in hiero.core.taskRegistry.localPresets():
+        if preset.name() == exportPresetName:
+            current_preset = preset
+            break
 
-  if not current_preset:
-    raise ValueError('No preset found for name : %s !' % presetName)
+    if not current_preset:
+        raise ValueError('No preset found for name : %s !' % presetName)
 
-  # Set the project root to a temp folder so we know where we output the files.
-  project = trackItem.project()
-  project.setProjectRoot( tempfile.tempdir )
+    # Set the project root to a temp folder so we know where we output the
+    # files.
+    project = trackItem.project()
+    project.setProjectRoot(tempfile.tempdir)
 
-  # get the entity reference to be used in the read node
-  entity_reference =  trackItem.source().entityReference()
+    # get the entity reference to be used in the read node
+    entity_reference = trackItem.source().entityReference()
 
-  def wrapCustomScriptWriter(*args, **kwargs):
-    return CustomScriptWriter(entity_reference=entity_reference)
+    def wrapCustomScriptWriter(*args, **kwargs):
+        return CustomScriptWriter(entity_reference=entity_reference)
 
-  # Replace the default ScriptWriter
-  hiero.core.nuke.ScriptWriter = wrapCustomScriptWriter
-  # Export the trackItem
-  hiero.core.taskRegistry.createAndExecuteProcessor(
-    current_preset,
-    [hiero.core.ItemWrapper(trackItem)],
-    synchronous=True
-  )
+    # Replace the default ScriptWriter
+    hiero.core.nuke.ScriptWriter = wrapCustomScriptWriter
+    # Export the trackItem
+    hiero.core.taskRegistry.createAndExecuteProcessor(
+        current_preset,
+        [hiero.core.ItemWrapper(trackItem)],
+        synchronous=True
+    )
 
-  script_path_result = CustomScriptWriter.result_outputs
-  FnAssetAPI.logging.info(script_path_result)
-  # Restore the default ScriptWriter
-  hiero.core.nuke.ScriptWriter = OriginalScriptWriter
+    script_path_result = CustomScriptWriter.result_outputs
+    FnAssetAPI.logging.info(script_path_result)
+    # Restore the default ScriptWriter
+    hiero.core.nuke.ScriptWriter = OriginalScriptWriter
 
-  # return the destination path of the nuke script
-  return script_path_result
-
-
-
-
+    # return the destination path of the nuke script
+    return script_path_result
