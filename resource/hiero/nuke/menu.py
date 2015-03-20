cf951eb49e75927cbb42a4047cd5b34017ac1006
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-03-20 17:47:58 +0100

    Added import of menu.py to register ftrack_connect_nuke

---
+++ resource/hiero/nuke/menu.py
@@ -0,0 +1,6 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2015 ftrack
+
+# Import ftrack_connect_nuke to make it available. This is required for usage in
+# e.g. definitions of knobs on the publish node.
+import ftrack_connect_nuke
