80eab3c5bc2a008e7f17a9b1df2f8c34c990056b
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-04-07 11:17:54 +0200

    Import host to allow build 76 to start

--- resource/hiero/nuke/postInit.py
+++ resource/hiero/nuke/postInit.py
@@ -4,5 +4,12 @@
 # This initializes the FnAssetAPI host
 import nuke.assetmgr
 import ftrack_connect_nuke_studio.plugin
+
+try:
+	# This is required to get build 76 to start.
+	import nuke.assetmgr.host
+except ImportError:
+	pass
+
 # Select a locally available manager
 nuke.assetmgr.start(ftrack_connect_nuke_studio.plugin.Plugin.getIdentifier())
